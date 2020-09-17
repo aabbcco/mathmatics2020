d19eac4664a99146f3725c7b482d141e486f3b69
aabbcco <fooly_big_gun@qq.com>
2020-09-17 21:40:26 +0800

    debug/add duplicated file

--- data_prep.py
+++ data_prep.py
@@ -2,13 +2,11 @@ import numpy as np
 import pandas as pd
 
 ptu1 = pd.read_excel("PTU_R06_12.xlsx")
-ptu2 = pd.read_excel("PTU_R06_15.xlsx")
 
 vis1=pd.read_excel("VIS_R06_12.xlsx")
-vis2=pd.read_excel("VIS_R06_15.xlsx")
 
 wind1 = pd.read_excel("WIND_R06_12.xlsx")
-wind2 = pd.read_excel("WIND_R06_15.xlsx")
+
 
 print('ptu',ptu1.columns)
 print('vis',vis1.columns)
@@ -17,34 +15,48 @@ print('winds',wind1.columns)
 #merge wind and vis
 data = pd.DataFrame()
 data["LOCALDATE (BEIJING)"] = vis1["LOCALDATE (BEIJING)"]
-data["LOCALDATE (BEIJING)"].append(vis2["LOCALDATE (BEIJING)"])
 data["MOR_1A"] = vis1["MOR_1A"]
-data["MOR_1A"].append(vis2["MOR_1A"])
 data["RVR_1A"] = vis1["RVR_1A"]
-data["RVR_1A"].append(vis2["RVR_1A"])
 data["WS2A (MPS)"] = wind1["WS2A (MPS)"]
-data["WS2A (MPS)"].append(wind2["WS2A (MPS)"])
 data["WD2A"] = wind1["WD2A"]
-data["WD2A"].append(wind2["WD2A"])
 data["CW2A (MPS)"] = wind1["CW2A (MPS)"]
-data["CW2A (MPS)"].append(wind2["CW2A (MPS)"])
 
-#merge ptu
-ptu1.append(ptu2)
+
+PAINS=[]
+QFE=[]
+QNH=[]
+TEMP=[]
+RH=[]
+DEWPOINT=[]
 
 ptu1_pointer = 0
-for i in range(data["LOCALDATE (BEIJING)"].shape[0]):
-    if (data["LOCALDATE (BEIJING)"][i]).strftime('%H%M') == (ptu1["LOCALDATE (BEIJING)"][ptu1_pointer]).strftime('%H%M'):
-        data["PAINS (HPA)"].append(ptu1["PAINS (HPA)"][ptu1_pointer])
-        data["QFE R06 (HPA)"].append(ptu1[ptu1["QFE R06 (HPA)"][ptu1_pointer]])
-        data["QNH AERODROME(HPA)"].append(ptu1[ptu1["QNH AERODROME(HPA)"][ptu1_pointer]])
-        data["TEMP (°C)"].append(ptu1[ptu1["TEMP (°C)"][ptu1_pointer]])
-        data["RH (%)"].append(ptu1[ptu1["RH (%)"][ptu1_pointer]])
-        data["DEWPOINT (掳C)"].append(ptu1[ptu1["DEWPOINT (掳C)"][ptu1_pointer]])
+data_pointer=0
+while(1):
+    if (data["LOCALDATE (BEIJING)"][data_pointer]).strftime('%H%M') == (ptu1["LOCALDATE (BEIJING)"][ptu1_pointer]).strftime('%H%M'):
+        PAINS.append(ptu1["PAINS (HPA)"][ptu1_pointer])
+        QFE.append(ptu1["QFE R06 (HPA)"][ptu1_pointer])
+        QNH.append(ptu1["QNH AERODROME (HPA)"][ptu1_pointer])
+        TEMP.append(ptu1["TEMP (°C)"][ptu1_pointer])
+        RH.append(ptu1["RH (%)"][ptu1_pointer])
+        DEWPOINT.append(ptu1["DEWPOINT (掳C)"][ptu1_pointer])
+        data_pointer+=1
+        if data_pointer == data["LOCALDATE (BEIJING)"].shape[0]:
+            break
     else:
         ptu1_pointer+=1
         print(ptu1_pointer)
 
-writer=pd.ExcelWriter('duplicated score')
+print(data["LOCALDATE (BEIJING)"].shape[0])
+
+data["PAINS (HPA)"]=PAINS
+data["QFE R06 (HPA)"]=QFE
+data["QNH AERODROME(HPA)"]=QNH
+data["TEMP (°C)"]=TEMP
+data["RH"]=RH
+data["DEWPOINT (掳C)"]=DEWPOINT
+
+writer=pd.ExcelWriter('./duplicated.xlsx')
 data.to_excel(writer)
-print("dones")
+writer.save()
+writer.close()
+print("done")
