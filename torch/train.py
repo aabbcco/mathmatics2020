88870e78842842b3c28b8952a9446ccd2b698d2b
aabbcco <fooly_big_gun@qq.com>
2020-09-18 22:24:16 +0800

    add layer

--- torch/train.py
+++ torch/train.py
@@ -5,11 +5,15 @@ class Model(nn.model):
     def __init__(self):
         super(Model, self).__init__()
         #conv layer1
-        self.conv1 = nn.Conv2d(3, 32, 3)
-        self.conv2 = nn.Conv2d(32, 64, 3)
-        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv1_1 = nn.Conv2d(3, 32, 3)
+        self.conv1_2 = nn.Conv2d(32, 64, 3)
+        self.conv1_3 = nn.Conv2d(64, 128, 3)
         #mlp layer1
         self.mlp1_1 = nn.Conv2d(128,256, 1)
         self.mlp1_2 = nn.Conv2d(256, 128, 1)
         self.mlp1_3 = nn.Conv2d(128, 64, 1)
+        #conv layer2
+        self.conv2_1 = nn.Conv2d(64, 128, 3)
+        self.conv2_2 = nn.Conv2d(128, 256, 3)
+        self.conv2_3 = nn.Conv2d(256, 512, 3)
         
